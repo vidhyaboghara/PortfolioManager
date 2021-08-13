9dc043b48d39fa0bf9fdafec2f9c302939b49319
vidhyaboghara <vidhya.boghara111@gmail.com>
2021-08-13 16:52:29 +0000

    mapping fixes

--- src/main/java/com/citi/hackathon/PortfolioManager/entities/Transaction.java
+++ src/main/java/com/citi/hackathon/PortfolioManager/entities/Transaction.java
@@ -29,14 +29,14 @@ public class Transaction implements Serializable {
     @Column(name = "user_id")
     private Integer userId;
 
-    @JoinColumn(name = "id", referencedColumnName = "user_id")
+    @JoinColumn(name = "id", referencedColumnName = "id", insertable = false,updatable = false)
     @ManyToOne( cascade={CascadeType.MERGE, CascadeType.PERSIST})
     private User user;
 
     @Column(name = "stock_id")
     private Integer stockId;
 
-    @JoinColumn(name = "stock_id", referencedColumnName = "stock_id")
+    @JoinColumn(name = "stock_id", referencedColumnName = "stock_id", insertable = false, updatable = false)
     @OneToOne( cascade={CascadeType.MERGE, CascadeType.PERSIST})
     private Stock stock;
 }
